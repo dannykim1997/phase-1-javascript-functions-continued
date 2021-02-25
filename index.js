fe119870716f6f9ea4009b5b36cd6eb7c5d6effc
github username <github email address>
2021-02-25 10:24:31 -0800

    commit tree 5b37876a3f67128d4f31e3fce2a14e426bc1956e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+const activity = 'roller-skate'
+
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (visual = "*") => function(adjective = "*") {
+    return `You are ${visual}${adjective}${visual}!`
+}
